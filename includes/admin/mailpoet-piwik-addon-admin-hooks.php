0c8f9917eeccf68131ffe076a523d141713ef914
Sebastien <sebastien@sebs-studio.com>
2014-02-18 11:28:52 +0100

    Got extended fields to load in step 3 of creating a newsletter

--- includes/admin/mailpoet-piwik-addon-admin-hooks.php
+++ includes/admin/mailpoet-piwik-addon-admin-hooks.php
@@ -12,21 +12,24 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-// Will run action and filter hooks if MailPoet is version 2.1.5
-if ( WYSIJA::get_version() >= '2.1.5' ) {
-	if ( isset( $_REQUEST['page'] ) ) {
+add_action( 'plugins_loaded', 'extend_newsletters' );
 
-		switch( $_REQUEST['page'] ) {
+function extend_newsletters() {
+	// Will run action and filter hooks if MailPoet is version 2.1.5
+	if ( WYSIJA::get_version() >= '2.1.5' ) {
+		if ( isset( $_REQUEST['page'] ) ) {
 
-			case 'wysija_campaigns':
-				$helper_extend_campaigns = WYSIJA::get('extend_campaigns', 'helper', false, WYSIJANLP);
-				add_filter('wysija_extend_step3', array( $helper_extend_campaigns, 'extend_step3_piwik_tracking' ) );
+			switch( $_REQUEST['page'] ) {
 
-			break;
+				case 'wysija_campaigns':
+					add_filter('wysija_extend_step3', 'extend_step3_piwik_tracking' );
 
-		} // end switch
+				break;
 
-	} // end if $_REQUEST['page']
+			} // end switch
 
-} // end if MailPoet version.
+		} // end if $_REQUEST['page']
+
+	} // end if MailPoet version.
+}
 ?>
