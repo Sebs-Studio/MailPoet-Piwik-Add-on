5db42d98f859471ec6e375a0cf4340bd28954985
seb86 <sebastien@sebs-studio.com>
2014-03-18 16:25:06 +0100

    Updated javascript

--- assets/js/admin/mailpoet-piwik-addon.js
+++ assets/js/admin/mailpoet-piwik-addon.js
@@ -8,12 +8,10 @@ jQuery(function(){
 
 	jQuery('input#piwikenabled').change(function() {
 		if (jQuery(this).is(':checked')) {
-			jQuery('#piwiksiteid').closest('tr').show();
 			jQuery('#piwikcampaignname').closest('tr').show();
 			jQuery('#piwikcampaignkeyword').closest('tr').show();
 		}
 		else {
-			jQuery('#piwiksiteid').closest('tr').hide();
 			jQuery('#piwikcampaignname').closest('tr').hide();
 			jQuery('#piwikcampaignkeyword').closest('tr').hide();
 		}
